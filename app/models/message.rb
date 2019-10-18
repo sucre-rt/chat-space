975325b245b0c8888141dae3b3afd6df94ec0a91
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-18 10:38:14 +0900

    message-model test

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,11 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
-  validates :content_or_image, presence: true
 
-  private
-
-  def content_or_image
-    content.presence or image.presence
+  validates :content, presence: true, unless: :image?
   end
-end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :mesage do
+  factory :message do
     content {Faker::Lorem.sentence}
     image {File.open("#{Rails.root}/public/images/test_image.jpg")}
     user
