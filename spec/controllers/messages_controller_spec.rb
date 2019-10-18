d89c490cb3f051f39313906722a22384dc6afe20
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-18 17:20:34 +0900

    messages_controller test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -35,4 +35,60 @@ describe MessagesController do
 
     end
   end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up message' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it 'redirects to group_messages_path' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context 'san not save' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id,message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it 'render index' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    context 'not log in' do
+      before do
+        get :create, params: params
+      end
+
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
 end
