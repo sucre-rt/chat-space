8ecb3b11bfd5045f99360576e6563d74e6ec79f1
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-20 20:54:47 +0900

    click function

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,20 +1,29 @@
 $(function() {
   let search_list = $("#user-search-result");
+  let group_list = $("#user-group-result");
 
   function appendUser(user) {
-    let html = 
-      `<div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">${ user.name }</p>
-      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
-      </div>`
-    search_list.append(html)
+    let html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
+                </div>`
+    search_list.append(html);
   }
 
   function appendErrMsgToHTML(msg) {
     let html = `<div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${ msg }</p>
-      </div>`;
+      </div>`
     search_list.append(html);
+  }
+
+  function appendGroup(name, id) {
+    let html = `<div class='chat-group-user'>
+                <input name='group[user_ids][]' type='hidden' value='${ id }'>
+                <p class='chat-group-user__name'>${ name }</p>
+                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+              </div>`
+    group_list.append(html);
   }
 
   $("#user-search-field").on("keyup", function(e) {
@@ -37,12 +46,20 @@ $(function() {
       } else {
         appendErrMsgToHTML("ユーザーが見つかりません")
       }
-      $('#user-search-result').on('click', '.chat-group-user__btn', function(){
-
-      });
     })
     .fail(function(){
       alert("ユーザー検索に失敗しました");
+    })
+  })
+  $(function(){
+    $(document).on('click', '.user-search-add', function(){
+      let id = $(this).data('user-id');
+      let name = $(this).data('user-name');
+      $(this).parent().remove();
+      appendGroup(name, id);
+    });
+    $(document).on("click",'.user-search-remove', function() {
+      $(this).parent().remove(); 
     })
   });
 });
