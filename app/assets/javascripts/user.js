e6f5b30b8aa02c18a7238b31fc4094efbc7b3c41
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-20 17:40:52 +0900

    create search-list

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,24 @@
 $(function() {
+  let search_list = $("#user-search-result");
 
-  $("#user-search-field").on("keyup", function() {
+  function appendUser(user) {
+    let html = 
+      `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${ user.name }</p>
+      <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
+      </div>`
+    search_list.append(html)
+  }
+
+  function appendErrMsgToHTML(msg) {
+    let html = `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${ msg }</p>
+      </div>`;
+    search_list.append(html);
+  }
+
+  $("#user-search-field").on("keyup", function(e) {
+    e.preventDefault();
     let input = $("#user-search-field").val();
 
     $.ajax({
@@ -8,6 +26,20 @@ $(function() {
       url: '/users',
       dataType: 'json',
       data: { keyword: input }
+    })
+
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      } else {
+        ppendErrMsgToHTML("ユーザーが見つかりません")
+      }
+    })
+    .fail(function(){
+      
     })
   });
 });
