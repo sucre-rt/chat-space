335b1da2789ba9311860d6674241ccdbae4a65ee
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-20 17:02:55 +0900

    create ajax data

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,5 +2,12 @@ $(function() {
 
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: { keyword: input }
+    })
   });
 });
