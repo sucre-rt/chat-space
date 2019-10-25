d74fb67e8214f0d62cdff1b8c7a4a283c943aed9
sucre-rt <sucre_fraises@ezweb.ne.jp>
2019-10-25 19:24:26 +0900

    repair

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,7 @@ class Api::MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     last_message_id = params[:last_message_id].to_i
-    @messages = @group.messages.includes(:user).where("id > #{last_message_id}")
+    @messages = @group.messages.includes(:user).where("id > #{last_message_id}").where.not(id: current_user.id)
     respond_to do |format|
       format.json    
       format.html
